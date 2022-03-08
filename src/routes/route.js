15e8f13a8c404cdc0c628be181602981c5fe43f0
minakshiMandoli <mandoliminakshi@gmail.com>
2022-03-08 10:25:23 -0800

    jwt assignment 8march

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const MiddleWare= require("../middleWare/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/createUsers", userController.createUser  )
 
-router.post("/login", userController.loginUser)
+router.post("/loginUser", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.get("/getUserData/:userId",MiddleWare.mid1, userController.getUserData)
 
+router.put("/updateUser/:userId",MiddleWare.mid1, userController.updateUser)
+router.delete("/deleteUser/:userId", MiddleWare.mid1,userController.deleteUser)
 module.exports = router;
