9b1e9227237cb327645365ae964bb3a2dab6f4fe
Manish-162403 <naamcool420@gmail.com>
2022-03-14 13:50:02 +0530

    update Models

---
+++ src/models/blogModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.types.ObjectId
+
+const blogSchema = new mongoose.Schema( {
+
+    title: {
+        required:true
+        },
+    body: {required:true},
+    authorId: {
+        type:ObjectId,
+        required:true,
+         ref: authorModel},
+    tags: [String],
+    category: {type:string,
+         required:true,
+         enum: [technology, entertainment, lifestyle, food, fashion]
+        }, 
+    subcategory: {type: string,
+        enum:[technology-["web development", "mobile development", AI, ML ]] },
+        isDeleted: {boolean, default: false}, 
+        publishedAt: {boolean, default: false},
+         isPublished: {boolean, default: false}
+
+    })
+
+    module.exports = mongoose.model('blogModel', authorSchema)
